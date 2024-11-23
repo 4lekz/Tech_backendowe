bee372678c688bfdb831c02ce4b059e41c4c116b
4lekz <mac.max.aleksander@gmail.com>
2024-11-23 22:25:06 +0100

    little changes

--- src/main/java/com/capgemini/wsb/fitnesstracker/notification/RaportGenerator.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/notification/RaportGenerator.java
@@ -1,5 +1,6 @@
 package com.capgemini.wsb.fitnesstracker.notification;
 
+import com.capgemini.wsb.fitnesstracker.mail.api.EmailDto;
 import com.capgemini.wsb.fitnesstracker.mail.api.EmailSender;
 import com.capgemini.wsb.fitnesstracker.training.api.Training;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingProvider;
@@ -16,10 +17,23 @@ private final TrainingProvider trainingProvider;
 
 private final EmailSender emailSender;
 
-    @Scheduled(cron="0 0 0 1 * ?")
-
+//    @Scheduled(cron="0 0 0 1 * ?")
+//    public void generateRaport() {
+//        List<Training> allTrainings =trainingProvider.getAllTrainings();
+//    }
+// Test email before setting it to monthly schedule
+    @Scheduled(cron = "0 */1 * * * ?")
     public void generateRaport() {
-        List<Training> allTrainings =trainingProvider.getAllTrainings();
+        List<Training> allTrainings = trainingProvider.getAllTrainings();
+
+        String content = allTrainings.toString();
+        emailSender.send(new EmailDto(
+                "test@example.com",
+                "Scheduled Report",
+                content
+        ));
+        System.out.println("Scheduled report email sent!");
     }
+
 
 }
